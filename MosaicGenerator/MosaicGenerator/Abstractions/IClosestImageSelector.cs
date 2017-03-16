7ee3f87f7dff970b1f840874b305ca7f8393f615
Goz3rr <wisse.aarts@gmail.com>
2017-03-16 14:13:09 +0100

    More interfaces = more better

---
+++ MosaicGenerator/MosaicGenerator/Abstractions/IClosestImageSelector.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.UI;
+
+namespace MosaicGenerator.Abstractions
+{
+    public interface IClosestImageSelector
+    {
+        Task<SoftwareBitmap> FindClosestImage(Color color, Dictionary<Color, List<SoftwareBitmap>> images);
+    }
+}
