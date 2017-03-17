54a2b9b32c719f56d5b8e681d9f374d81a67696c
Goz3rr <wisse.aarts@gmail.com>
2017-03-17 15:51:15 +0100

    ImageGenerator dingen

---
+++ MosaicGenerator/MosaicGenerator/Abstractions/IImageSaver.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+
+namespace MosaicGenerator.Abstractions
+{
+    public interface IImageSaver
+    {
+        Task SaveImageAsync(SoftwareBitmap image);
+    }
+}
---
+++ MosaicGenerator/MosaicGenerator/Implementations/ImageGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MosaicGenerator.Abstractions;
+using Windows.Graphics.Imaging;
+using Windows.UI;
+
+namespace MosaicGenerator.Implementations
+{
+    public class ImageGenerator : IImageGenerator
+    {
+        private readonly IClosestImageSelector closestImageSelector;
+        private readonly IAverageColorCalculator averageColorCalculator;
+
+        public ImageGenerator(IClosestImageSelector closestImageSelector, IAverageColorCalculator averageColorCalculator)
+        {
+            this.closestImageSelector = closestImageSelector;
+            this.averageColorCalculator = averageColorCalculator;
+        }
+
+        public async Task<SoftwareBitmap> GenerateImage(SoftwareBitmap image, Dictionary<Color, List<SoftwareBitmap>> lookup, int blockSize)
+        {
+            var output = new SoftwareBitmap(BitmapPixelFormat.Rgba8, image.PixelWidth, image.PixelHeight);
+
+            var blocks = await averageColorCalculator.CalculateAverage(image, blockSize);
+
+            for(int i = 0; i < blocks.Length; i++)
+            {
+                var closest = await closestImageSelector.FindClosestImage(blocks[i], lookup);
+            }
+
+            return output;
+        }
+    }
+}
